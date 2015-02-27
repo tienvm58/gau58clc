4fa919d9c30b0941827054ccfd5a5f347446c222
cuongnv58 <hanhphuclahappy@gmail.com>
2015-02-27 22:02:49 +0700

    Update menu.php
    fix

--- GAU_CLC/menu.php
+++ GAU_CLC/menu.php
@@ -7,27 +7,11 @@
 	</head>
 		
 	<body>
-		<div class="cover"></div>
-    		<div class="khungchinh" >
-        		<ul>
-						<li><a href="Code.html">Home</a></li>
-            			<li><a href="Menu.php">Menu</a></li>
-            			<li><a href="#">Đặt hàng</a></li>
-						<li><a href="#">Quản lí</a>
-						<ul>
-							<li><a href="Add.php">Thêm Món</a></li>
-							<li><a href="#">Xóa</a></li>
-						</ul>
-            			<li><a href="#">Liên hệ</a></li>
-            			<li><a href="#">Góp ý</a></li>
-        		</ul>
-    
-		 </div>
 		
 		
         
         <?php
-
+	include 'head.html';
         $connection = mysql_connect("localhost","root","") or die ("Không thể kết nối đến dữ liệu");
         mysql_select_db("nhahang",$connection);
         $sql = "select * from khaivi";
@@ -137,34 +121,7 @@
         
         <?php
 			mysql_close($connection);
-		?>
-
-        
-		
-		<br><br><br><br><br><br><br><br><br><br>
-		<div align = "center">
-		<table cellspacing = "20" border = "0">
-			<tr>
-				<div align = "center">
-				<table border = "1px" width = "994px" height = "100px" align = "center" cellspacing = "5px">
-					<tr>
-						<br><br><br>
-						<td><div align = "left"> 
-							<ul>
-								<li> <img src = "Image/footer.png " width = "100px" height = "100px"> </li>
-							</ul>
-						</div></td>
-						<td><div align = "center-leftright">
-							<ul>
-								<li> Địa chỉ: 144 Xuân Thủy, Cầu Giấy, Hà Nội </li>
-								<li> Điện thoại: 0912345678 </li>
-								<li> Facebook:<li>
-								<li> <a href = "Code.html">Về trang chủ</a></li>
-							</ul>
-						</div></td>
-						</tr>
-				</table>
-			</tr>
-		</table>	
+			include 'end.html';
+	?>
 	</body>
 </html>
