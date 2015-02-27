cd4c636a0dac783a8875c3a2b0d370fb736210e4
cuongnv58 <hanhphuclahappy@gmail.com>
2015-02-27 22:04:23 +0700

    Update and rename Intro.html to Intro.php

---
+++ GAU_CLC/Intro.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+		<meta name="author" content="GallerySoft.info" />
+    		<link rel="stylesheet" href="style.css" type="text/css" />
+		<title>	Welcome to Gấu's restaurant	</title>
+	</head>
+		
+	<body>
+		<?php
+			include 'head.html';
+		?>
+		<?php
+			include 'end.html';
+		?>
+			
+	</body>
+</html>
