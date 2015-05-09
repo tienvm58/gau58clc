ebb202f73477055372d83d9114fc9e98b38e2649
chinhpv95 <chinhpv95@gmail.com>
2015-05-09 18:34:58 +0700

    Update gopy_user.php

--- GAU_CLC/gopy_user.php
+++ GAU_CLC/gopy_user.php
@@ -12,19 +12,25 @@
 			include 'head_user.html'
 		?>
 		<br><br><br><br>
-		<form name="gopyus" action="gopyuser.php" method="post" >
-			<div class = "comment">
-				<h2>Nhận xét của bạn về chất lượng của nhà hàng </h2><br>
-				<br><b>Nhận xét của bạn </b><br>
-				<textarea rows = "8" cols = "60" name="gopy" class = "box_gopy" id="gopy" required/></textarea>
-				<br><br>
-				<input type="submit" name="ok" class = "button_action" /> <br><br>
-			</div>
-		</form>
-		<div align = "center">
-			<?php
-				include 'end.html';
-			?>
+		
+		<div class = "container">
+			<form name="gopyus" action="gopyuser.php" method="post" >
+				<div class = "comment">
+					<div class = "form-group">
+					<h3>Nhận xét của bạn về chất lượng của nhà hàng </h3><br>
+					<b>Tên của bạn</b><br>
+					<input type="text" name="usname"maxlength = "25" required/> <br><br>
+					<br><b>Nhận xét của bạn </b><br>
+					<textarea rows = "8" cols = "120" name="gopy" class = "box_gopy" id="gopy" required/></textarea>
+					<br><br>
+					<input type="submit" name="ok" class = "btn" /> <br><br>
+					</div>
+				</div>
+			</form>
 		</div>
+			
+		<?php
+			include 'end.html';
+		?>
 	</body>
 </html>
