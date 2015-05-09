9f383296e95dd58676e138920543436a86a1fe22
cuongnv58 <hanhphuclahappy@gmail.com>
2015-05-09 17:27:23 +0700

    Đơn đặt món mà admin nhìn thấy

---
+++ GAU_CLC/Menu_order_admin.php
@@ -0,0 +1,50 @@
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+		<meta name="author" content="GallerySoft.info" />
+    		<link rel="stylesheet" href="style.css" type="text/css" />
+		<title>	Thêm món ăn	</title>
+	</head>
+	<body>
+		<?php
+		include 'head_admin.html'
+		?>
+		<?php
+		$conn=mysql_connect("localhost", "root", "") or die("failed");
+		mysql_select_db("nhahang",$conn);
+		$sql ="select * from offer order by Time desc";
+		$query=mysql_query($sql);
+		header("content-type:text/html;charset=utf-8");
+		//$data= mysql_fetch_assoc($query);
+		echo "<br><br><br>";
+		echo"<div align='center'>";
+		echo"<table border='1' align='center' weight='1000' height='200' >";
+		echo "<tr class='tit'>";
+			echo"<td>Thời gian</td>";
+			echo"<td>Bàn</td>";
+			echo"<td>Tên món</td>";
+			echo"<td>Đơn giá</td>";
+			echo"<td>Số lượng</td>";
+			echo"<td>Thành tiền</td>";
+		echo "</tr>";
+		while($data=mysql_fetch_assoc($query)){
+			echo "<tr>";
+				echo"<td>".$data["Time"]."</td>";
+				echo"<td>".$data["User"]."</td>";
+				echo"<td>".$data["Name"]."</td>";
+				echo"<td>".$data["Price"]."</td>";
+				echo"<td>".$data["Qty"]."</td>";
+				echo"<td>".$data["Money"]."</td>";
+			echo "</tr>";
+		}
+		echo"</table>";
+		echo"</div>";
+	?>
+	<div align = "center">
+		<?php
+			include 'end.html';
+		?>
+	</div>
+	</body>
+	</body>
+</html>		
