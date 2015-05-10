a867f0a91ab937a07b8269ee2f8b2eef28a64cea
Tienvm <tienvm_58@vnu.edu.vn>
2015-05-10 23:13:05 +0700

    sync

--- GAU_CLC/index.php
+++ GAU_CLC/index.php
@@ -2,10 +2,14 @@
 	session_start();
 	if(isset($_SESSION['name']) && $_SESSION['level'] == 2)
 		{				
-		echo '<meta http-equiv="refresh" content="0; url=Code_user.php">';
+			echo '<meta http-equiv="refresh" content="0; url=Code_user.php">';
 		}
 	else if(isset($_SESSION['name']) && $_SESSION['level'] == 1)
 		{			
 			echo '<meta http-equiv="refresh" content="0; url=Code_admin.php">';
+		}
+	else 
+		{
+			echo '<meta http-equiv="refresh" content="0; url=Code.php">';
 		}
 ?>
