4547caa10f42d7fc4afeafac1a598d806c828142
Tienvm <tienvm_58@vnu.edu.vn>
2015-05-11 00:48:07 +0700

    cap nhat logo tab

--- GAU_CLC/Code.php
+++ GAU_CLC/Code.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
+		<?php include"logo_tab.php" ?>
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 			<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> -->
 		<title> Welcome to Gấu's Restaurant </title>
---
+++ GAU_CLC/logo_tab.php
@@ -0,0 +1,3 @@
+<?php
+	echo '<link rel="shortcut icon" href="image\BEAR-LOGO.png"/>';
+?>
